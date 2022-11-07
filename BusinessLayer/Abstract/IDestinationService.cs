c3a2cebf10cef04a7f7bc2412335beda8f1344dc
acarnilsu <nilsuacar42@gmail.com>
2022-11-07 22:56:00 +0300

    UPDATE

--- BusinessLayer/Abstract/IDestinationService.cs
+++ BusinessLayer/Abstract/IDestinationService.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Abstract
+namespace BusinessLayer.Abstract
 {
-    public interface IFeature2Dal : IGenericDal<Feature2>
+    public interface IDestinationService : IGenericService<Destination>
     {
     }
 }
--- DataAccessLayer/Abstract/IAboutDal.cs
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface IAboutDal : IGenericDal<Destination>
+    public interface IAboutDal : IGenericDal<About>
     {
     }
 }
