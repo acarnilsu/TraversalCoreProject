5713635ffb0df03a8c12b0d49060bd09e5c0120d
acarnilsu <nilsuacar42@gmail.com>
2022-12-31 23:51:48 +0300

    View Components

---
+++ TraversalCoreProject/ViewComponents/Default/_PopularDestinations.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _PopularDestinations:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {    
+            return View();
+        }
+    }
+}
